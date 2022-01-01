0aa48661e86c5ed26685969e2e6131ef7c42631c
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2021-12-31 19:30:56 -0700

    feat: render counter class dynamically

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -8,12 +8,16 @@ class Counter extends React.Component {
   render() {
     return (
       <div>
-        <span className="badge bg-primary m-2">
-          {this.formatCount()}
-        </span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 bg-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
